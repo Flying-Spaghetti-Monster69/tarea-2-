b4d662e15885fe37b9758fcca6b770352a7d3c2a
Felipe Garzon Melguizo <felipegarzon908@gmail.com>
2023-08-29 21:25:39 -0500

    ejercicio 12 de capitulo 4 resuelto

---
+++ ejercicios capitulo 4/Ejercicio 12.py
@@ -0,0 +1,13 @@
+nombre = input("ingrese el nombre del trabajador: ")
+horasTrabajadas = float(input("ingrese el numero de horas trabajadas: "))
+valorPorHora = float(input("ingrese el valor de cada hora trabajada: "))
+
+if (horasTrabajadas > 40):
+    if (horasTrabajadas > 48):
+        devengo = 40*valorPorHora + 8*2*valorPorHora + (horasTrabajadas - 48)*3*valorPorHora
+    else:
+        devengo = 40*valorPorHora+(horasTrabajadas - 40)*2*valorPorHora
+else:
+    devengo = horasTrabajadas*valorPorHora
+
+print("el trabajador",nombre,"devengo: $",devengo)
